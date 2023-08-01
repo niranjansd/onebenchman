33ff903c00a28eeea2ff53ab140a8fe26373c803
niranjansd <niranjan.sd@gmail.com>
2023-08-01 00:35:47 -0700

    Glue dataset definition

---
+++ dataset_lib.py
@@ -0,0 +1,16 @@
+
+
+# Define datasets and features
+
+GLUE = {
+    'cola': ['sentence'],
+    'sst2': ['sentence'],
+    'mrpc': ['sentence1', 'sentence2'],
+    'qqp': ['question1', 'question2'],
+    'stsb': ['sentence1', 'sentence2'],
+    'mnli': ['premise', 'hypothesis'],
+    'ax': ['premise', 'hypothesis'],
+    'qnli': ['question', 'sentence'],
+    'rte': ['sentence1', 'sentence2'],
+    'wnli': ['sentence1', 'sentence2'],
+    }
